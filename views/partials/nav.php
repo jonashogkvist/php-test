4710393c9bb2be301cc58a31a23af97c72fa884d
jonashogkvist <166859046+jonashogkvist@users.noreply.github.com>
2024-04-25 02:55:50 +0200

    Split up the views into partials

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -1,16 +1,3 @@
-<!doctype html>
-<html lang="en" class="h-full bg-gray-100">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Demo</title>
-    <script src="https://cdn.tailwindcss.com"></script>
-</head>
-<body class="h-full>
-
-<div class="min-h-full">
 <nav class="bg-gray-800">
     <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div class="flex h-16 items-center justify-between">
@@ -155,22 +142,3 @@
         </div>
     </div>
 </nav>
-
-<header class="bg-white shadow">
-    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        <h1 class="text-3xl font-bold tracking-tight text-gray-900">About Us</h1>
-    </div>
-</header>
-<main>
-    <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-        <p>
-            About Page
-        </p>
-    </div>
-</main>
-</div>
-
-
-</body>
-</html>
-
