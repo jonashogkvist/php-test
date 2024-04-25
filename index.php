443fb41a237bfc62b1474766f79c6348eb76d343
jonashogkvist <166859046+jonashogkvist@users.noreply.github.com>
2024-04-25 02:01:19 +0200

    Test functions

--- index.php
+++ index.php
@@ -15,30 +15,44 @@
             [
                 "name" => "Do Androids Dream of Electric Sheep",
                 "author" => "Philip K. Dick",
+                "releaseYear" => "1968",
                 "purchaseUrl" => "https://example.com"
             ],
             [
-                "name" => "The Langoliers",
-                "author" => "Philip K. Dick",
+                "name" => "Project Hail Mary",
+                "author" => "Andy Weir",
+                "releaseYear" => "2021",
                 "purchaseUrl" => "https://example.com"
             ],
             [
-                "name" => "Hail Mary",
-                "author" => "Philip K. Dick",
+                "name" => "The Martian",
+                "author" => "Andy Weir",
+                "releaseYear" => "2011",
                 "purchaseUrl" => "https://example.com",
             ]
-        ]
+        ];
+
+
+    function filterByAuthor($books, $author): array
+    {
+        $filteredBooks = [];
+
+        foreach ($books as $book){
+            if ($book['author'] === $author) {
+                $filteredBooks[] = $book;
+            }
+        }
+
+        return $filteredBooks;
+    }
 ?>
 
     <ul>
-
-        <?php foreach ($books as $book) : ?>
+        <?php foreach (filterByAuthor($books, "Andy Weir") as $book) : ?>
             <li>
-                <a href="#"> <?= $book['name']; ?> </a>
+                <a href="#"> <?= $book['name']; ?> (<?= $book['releaseYear'] ?>) - By <?= $book['author'] ?></a>
             </li>
         <?php endforeach; ?>
-
-
     </ul>
 
 </body>
