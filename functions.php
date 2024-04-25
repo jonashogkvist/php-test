3cc3caccf5f7b4e53dbfef9d0862fb9cfa75b2a7
jonashogkvist <166859046+jonashogkvist@users.noreply.github.com>
2024-04-25 03:15:46 +0200

    Clean up index.php

--- functions.php
+++ functions.php
@@ -1,6 +1,16 @@
 <?php
 
+use JetBrains\PhpStorm\NoReturn;
+
 function urlIs($value): bool
 {
     return $_SERVER['REQUEST_URI'] === $value;
+}
+
+#[NoReturn] function dd($value): void
+{
+    echo "<pre>";
+    var_dump($value);
+    echo "<pre>";
+    die();
 }
--- index.php
+++ index.php
@@ -3,14 +3,4 @@ require "functions.php";
 
 $heading = "Home";
 
-function dd($value) {
-    echo "<pre>";
-    var_dump($value);
-    echo "<pre>";
-    die();
-}
-
-
-
-
 require "views/index.view.php";
