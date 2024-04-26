861565d7d32eb1e1a30756cd37612a434326c22a
jonashogkvist <166859046+jonashogkvist@users.noreply.github.com>
2024-04-26 02:18:21 +0200

    Connect to wsl2 mysql db

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php
+require "config.php";
 require "functions.php";
 require "router.php";
+
+
+$dsn = "mysql:host=localhost;port=33061;dbname=myapp;charset=utf8mb4;user=$DB_USERNAME;password=$DB_PASSWORD";
+$pdo = new PDO($dsn);
+
+$statement = $pdo->prepare("select * from posts");
+
+$statement->execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($posts as $post) {
+    echo "<li>" . $post['title'] . "</li>";
+}
