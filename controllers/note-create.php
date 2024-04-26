d8dd9b82423c32c51cfd6248cca61b860977fffb
jonashogkvist <166859046+jonashogkvist@users.noreply.github.com>
2024-04-26 07:03:21 +0200

    Escape untrusted input

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -1,9 +1,15 @@
 <?php
 
+$config = require "config.php";
+$db = new Database($config['database'], $config['username'], $config['password']);
+
 $heading = "Create Note";
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    dd("You submitted the form");
+    $db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)', [
+        'body' => $_POST['body'],
+        'user_id' => 5,
+    ]);
 }
 
 require 'views/note-create.view.php';
