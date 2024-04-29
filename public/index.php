c4492b61e22bb6d3f64c3e6d5c9224fe6f2dc58f
jonashogkvist <166859046+jonashogkvist@users.noreply.github.com>
2024-04-29 13:18:54 +0200

    Move classes to Core dir, update autoload

--- public/index.php
+++ public/index.php
@@ -5,10 +5,8 @@ const BASE_PATH = __DIR__ . '/../';
 require BASE_PATH . "functions.php";
 
 spl_autoload_register(function ($class) {
-    require (base_path($class . '.php'));
+    require (base_path("Core/{$class}.php"));
 });
 
 require base_path("router.php");
-
-
 
