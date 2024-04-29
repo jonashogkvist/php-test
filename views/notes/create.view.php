2cb9816344ff317e884d8ae5e65b1969deb2a615
jonashogkvist <166859046+jonashogkvist@users.noreply.github.com>
2024-04-29 12:33:47 +0200

    Restructure notes pages

--- views/notes/create.view.php
+++ views/notes/create.view.php
@@ -1,6 +1,6 @@
-<?php require('partials/head.php'); ?>
-<?php require('partials/nav.php'); ?>
-<?php require('partials/banner.php'); ?>
+<?php require('views/partials/head.php'); ?>
+<?php require('views/partials/nav.php'); ?>
+<?php require('views/partials/banner.php'); ?>
 
 <main>
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
@@ -15,7 +15,7 @@
                                           rows="3"
                                           class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                           required
-                                > <?= $_POST['body'] ?? ''; ?></textarea>
+                                ><?=$_POST['body'] ?? '';?></textarea>
 
                                 <?php if (isset($errors['body'])) : ?>
                                     <p class="text-red-500 text-xs mt-2"> <?= $errors['body'] ?> </p>
@@ -34,5 +34,5 @@
     </div>
 </main>
 
-<?php require('partials/footer.php'); ?>
+<?php require('views/partials/footer.php'); ?>
 
